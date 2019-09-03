ab94441c2e8b8f4cb138a6dbcfda64fc96244455
Luca Salzani <luca.salzani@zuehkle.com>
2019-09-03 15:19:55 +0200

    Configure azure as backend

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  communicationHubBaseUrl: 'https://localhost:5001/',
+  communicationHubBaseUrl: 'https://planningpokr-backend.azurewebsites.net/', // 'https://localhost:5001/',
   communicationHubPath: 'communicationHub'
 };
